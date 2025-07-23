0d3986a9b15dc7d04488e51fc685bf17db7b5586
Darshitsepl <sepl.darshittarapara@gmail.com>
2025-07-23 21:06:34 +0530

    Make a ISR Type testing

--- app/(root)/authors/page.tsx
+++ app/(root)/authors/page.tsx
@@ -5,7 +5,7 @@ import { getAllAuthor } from "@/sanity/lib/query/query";
 import { writeClient } from "@/sanity/lib/writeclient";
 
 // Enable ISR (optional)
-//export const revalidate = 1; // regenerate every 60 seconds
+export const revalidate = 10; // regenerate every 60 seconds
 
 const Authors = async () => {
   const data = await writeClient.fetch(getAllAuthor);
